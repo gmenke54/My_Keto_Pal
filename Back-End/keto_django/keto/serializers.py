a2d7dcfa0e4081ef055ed981f7a3444133985241
gmenke54 <menke.gw@gmail.com>
2022-02-07 14:04:46 -0800

    debugged DaySerializer

--- Back-End/keto_django/keto/serializers.py
+++ Back-End/keto_django/keto/serializers.py
@@ -1,22 +1,73 @@
 from rest_framework import serializers
 from .models import User, Profile, Day, Meal, Food
 
+# BROKEN:
+# class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+#     days = serializers.HyperlinkedRelatedField(
+#         view_name='day_detail',
+#         many=True,
+#         read_only=True
+#     )
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-    days = serializers.HyperlinkedRelatedField(
-        view_name='day_detail',
+#     profile_url = serializers.ModelSerializer.serializer_url_field(
+#         view_name='profile_detail'
+#     )
+
+#     user = serializers.HyperlinkedRelatedField(
+#         view_name='user_detail',
+#         read_only=True
+#     )
+
+#     user_id = serializers.PrimaryKeyRelatedField(
+#         queryset=User.objects.all(),
+#         source='user'
+#     )
+
+#     class Meta:
+#         model = Profile
+#         fields = ('id', 'profile_url', 'user', 'user_id', 'cur_weight',
+#                   'goal_weight', 'img', 'keto_weeks', 'days', )
+
+
+# class DaySerializer(serializers.HyperlinkedModelSerializer):
+#     meals = serializers.HyperlinkedRelatedField(
+#         view_name='meal_detail',
+#         many=True,
+#         read_only=True
+#     )
+
+#     day_url = serializers.ModelSerializer.serializer_url_field(
+#         view_name='day_detail'
+#     )
+
+#     user = serializers.HyperlinkedRelatedField(
+#         view_name='user_detail',
+#         read_only=True
+#     )
+
+#     user_id = serializers.PrimaryKeyRelatedField(
+#         queryset=User.objects.all(),
+#         source='user'
+#     )
+
+#     class Meta:
+#         model = Day
+#         fields = ('id', 'day_url', 'user', 'user_id', 'date', 'meals', )
+
+class ProfileSerializer(serializers.ModelSerializer):
+    days = serializers.PrimaryKeyRelatedField(
+        queryset=Day.objects.all(),
         many=True,
-        read_only=True
     )
 
     profile_url = serializers.ModelSerializer.serializer_url_field(
         view_name='profile_detail'
     )
 
-    user = serializers.HyperlinkedRelatedField(
-        view_name='user_detail',
-        read_only=True
-    )
+    # user = serializers.HyperlinkedRelatedField(
+    #     view_name='user_detail',
+    #     read_only=True
+    # )
 
     user_id = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(),
@@ -25,25 +76,24 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ('id', 'profile_url', 'user', 'user_id', 'cur_weight',
+        fields = ('id', 'profile_url', 'user_id', 'cur_weight',
                   'goal_weight', 'img', 'keto_weeks', 'days', )
 
 
-class DaySerializer(serializers.HyperlinkedModelSerializer):
-    meals = serializers.HyperlinkedRelatedField(
-        view_name='meal_detail',
+class DaySerializer(serializers.ModelSerializer):
+    meals = serializers.PrimaryKeyRelatedField(
+        queryset=Meal.objects.all(),
         many=True,
-        read_only=True
     )
 
     day_url = serializers.ModelSerializer.serializer_url_field(
         view_name='day_detail'
     )
 
-    user = serializers.HyperlinkedRelatedField(
-        view_name='user_detail',
-        read_only=True
-    )
+    # user = serializers.HyperlinkedRelatedField(
+    #     view_name='user_detail',
+    #     read_only=True
+    # )
 
     user_id = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(),
@@ -52,9 +102,10 @@ class DaySerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Day
-        fields = ('id', 'day_url', 'user', 'user_id', 'date', 'meals', )
+        fields = ('id', 'day_url', 'user_id', 'date', 'meals', )
 
 
+# Working:
 class FoodSerializer(serializers.ModelSerializer):
     meals = serializers.PrimaryKeyRelatedField(
         queryset=Meal.objects.all(),
