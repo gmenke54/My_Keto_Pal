a67010623a0c4cdcf238d720b194c7ba3751e273
gmenke54 <menke.gw@gmail.com>
2022-02-09 21:39:43 -0800

    removed unneccesary comments

--- Back-End/keto_django/keto/admin.py
+++ Back-End/keto_django/keto/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, Day, Food, Recipe, Comment
 
-# Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Day)
--- Back-End/keto_django/keto/models.py
+++ Back-End/keto_django/keto/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import datetime
 
-# Create your models here.
-
 
 class User(AbstractUser):
     pass
